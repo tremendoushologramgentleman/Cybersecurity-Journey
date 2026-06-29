67d735fbecec0cc962d95ddd8b46f68b3e0026a8
tremendoushologramgentleman <rahulsonu06@hotmail.com>
2026-06-29 08:21:47 -0500

    if-else conditionv2

---
+++ python/q4.1.py
@@ -0,0 +1,7 @@
+username=input("Enter your username:")
+password=input("Enter your password:")
+if (username=="scarface" and  password=="scarface123"):
+	print("Welcome Scarface,System unlocked")
+else:
+	print("Access Denied")
+
