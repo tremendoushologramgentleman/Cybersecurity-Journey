7166dbdb28573be65755f74e2af5dd68961b1d88
tremendoushologramgentleman <rahulsonu06@hotmail.com>
2026-06-29 08:11:21 -0500

    if-else condition

---
+++ python/q4.py
@@ -0,0 +1,6 @@
+username=(input("Enter the username:"))
+password=(input("Enter the password:"))
+if (password=="scarface123"):
+	print("Access Granted , Welcome hacker")
+else:
+	print("Access Denied , Wrong password")
